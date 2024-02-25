9d112a6d211ee1246d7b57108fcaafe66c44e78d
Ewan <ebreakey@gmail.com>
2024-02-26 10:44:53 +1100

    Include types

---
+++ index.d.mts
@@ -0,0 +1,3 @@
+export default function rehypeToText(this: import("unified").Processor<undefined, undefined, undefined, undefined, undefined>, ...parameters: [import("hast-util-to-text/lib").Options] | void[]): void | undefined;
+export type Root = import('hast').Root;
+export type Options = import('hast-util-to-text').Options;
--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ import { toText } from 'hast-util-to-text'
 /** Compiler plugin which converts hast tree to text using equivalent of .textContent algorithm
  * @type {import('unified').Plugin<[Options]|void[], Root, string>} */
 export default function rehypeToText(options) {
-  /** @type {import('unified').CompilerFunction<Root, string>} */
+  /** @type {import('unified').Compiler<Root, string>} */
   const compiler = (tree) => {
     const settings = /** @type {Options} */ (this.data('settings'))
 
